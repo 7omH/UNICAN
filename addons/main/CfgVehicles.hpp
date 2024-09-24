65cbc1bd11ac80c1cfccc2a3a611bdcb5164f111
Tom <kraken.to.eat@gmail.com>
2024-09-24 11:10:04 +0200

    Added Item UNICAN

--- addons/main/CfgVehicles.hpp
+++ addons/main/CfgVehicles.hpp
@@ -7,44 +7,44 @@ class CfgVehicles {
 					displayName="UNICAN";
 					condition="('TOMH_UNICAN' in (items player))";
 					exceptions[]={};
-					icon = "spray\UNICAN.paa";
+					icon = "\UNICAN\addons\spray\UNICAN.paa";
 					#include "mastercan.hpp"
 				};
 				class TOMH_UNICAN_Lite_Alpha {
 					displayName = "Alpha";  
 					requiredItem = "TOMH_UNICAN_Lite";  
-					textures[] = {"spray\AOOO.paa"}; 
-					icon = "spray\AOOO.paa";  
+					textures[] = {"\UNICAN\addons\spray\AOOO.paa"}; 
+					icon = "\UNICAN\addons\spray\AOOO.paa";  
 				};
 				class TOMH_UNICAN_Lite_Bravo {
 					displayName = "Bravo";  
 					requiredItem = "TOMH_UNICAN_Lite";  
-					textures[] = {"spray\BOOO.paa"}; 
-					icon = "spray\BOOO.paa";  
+					textures[] = {"\UNICAN\addons\spray\BOOO.paa"}; 
+					icon = "\UNICAN\addons\spray\BOOO.paa";  
 				};
 				class TOMH_UNICAN_Lite_Rearm {
 					displayName = "Rearm";  
 					requiredItem = "TOMH_UNICAN_Lite";  
-					textures[] = {"spray\OROO.paa"}; 
-					icon = "spray\OROO.paa";  
+					textures[] = {"\UNICAN\addons\spray\OROO.paa"}; 
+					icon = "\UNICAN\addons\spray\OROO.paa";  
 				};
 				class TOMH_UNICAN_Lite_Repair {
 					displayName = "Repair";  
 					requiredItem = "TOMH_UNICAN_Lite";  
-					textures[] = {"spray\OORO.paa"}; 
-					icon = "spray\OORO.paa";  
+					textures[] = {"\UNICAN\addons\spray\OORO.paa"}; 
+					icon = "\UNICAN\addons\spray\OORO.paa";  
 				};
 				class TOMH_UNICAN_Lite_Repair {
 					displayName = "Repair";  
 					requiredItem = "TOMH_UNICAN_Lite";  
-					textures[] = {"spray\OOOR.paa"}; 
-					icon = "spray\OOOR.paa";  
+					textures[] = {"\UNICAN\addons\spray\OOOR.paa"}; 
+					icon = "\UNICAN\addons\spray\OOOR.paa";  
 				};
 				class TOMH_UNICAN_Lite_RedCross {
 					displayName = "Red Cross";  
 					requiredItem = "TOMH_UNICAN_Lite";  
-					textures[] = {"spray\RedCross.paa"}; 
-					icon = "spray\RedCross.paa";  
+					textures[] = {"\UNICAN\addons\spray\RedCross.paa"}; 
+					icon = "\UNICAN\addons\spray\RedCross.paa";  
 				};
 			};
 		};
@@ -53,6 +53,12 @@ class CfgVehicles {
 	class TOMH_Item_UNICAN:ACE_Item_SpraypaitnBlack {
 		author = "TomH";
 		displayName = "UNICAN";
+		class TransportItems {
+			class _xx_Item {
+				name = "TOMH_UNICAN";
+				count = 1;
+			};
+		};
 	};
 	
 };
