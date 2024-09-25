2df19e0ae6024e6e0fb4701a744b6edf138e0fbb
7omH <149675034+7omH@users.noreply.github.com>
2024-09-25 19:50:04 +0200

    Generated station variants

--- addons/main/spray/StationMatrix.ps1
+++ addons/main/spray/StationMatrix.ps1
@@ -6,12 +6,12 @@ magick composite OROO.png OORR.png ORRR.png
 # generate variants
 $station = @('A','B','C','D','E','F')
 $base = @("AOOO.png","BOOO.png","COOO.png","DOOO.png","EOOO.png","FOOO.png")
-$modif = @("OROO.png","OORO.png","ORRO.png","OOOR.png","OROR.png","ORRR.png")
+$modif = @("OROO.png","OORO.png","ORRO.png","OOOR.png","OROR.png","OORR.png""ORRR.png")
 
 foreach($b in $base) {
 	
 	foreach($c in $modif) {
 		$variant = -join($b.Substring(0,1),$c.Substring(1,7))
-		magick composite $b $c $variant
+		echo $variant
 	}
 }
