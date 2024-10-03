15e95f0286e00247d0c043aaedfc9fd955ffe508
Tom <kraken.to.eat@gmail.com>
2024-10-03 13:51:38 +0200

    Added mod.cpp info

---
+++ mod.cpp
@@ -0,0 +1,7 @@
+name = "UNICAN";
+hidePicture = "true";
+overview = "UNICAN adds new tags and items for marking";
+tooltip = "All you want to do in a UNICAN";
+tooltipOwned = "TOMH_UNICAN Owned";
+logo = "unican.paa";
+author = "TomH";
