d8ba4b0459d53d55d802836d8b3a5eb2bb3e6b98
7omH <149675034+7omH@users.noreply.github.com>
2025-01-27 09:25:44 +0100

    Added Logistics variant of mastercan

--- addons/main/CfgVehicles.hpp
+++ addons/main/CfgVehicles.hpp
@@ -2,12 +2,19 @@ class CfgVehicles {
 	class Man;
 	class CAManBase: Man {
 		class ACE_SelfActions {
-			class UNICAN {
+			class UNICAN_mastercan {
 				displayName="UNICAN";
 				condition="('TOMH_UNICAN' in (items player))";
 				exceptions[]={};
 				icon = QPATHTOF(data\unican.paa);
 				#include "mastercan.hpp"
+			};
+			class UNICAN_logican {
+				displayName="UNICAN";
+				condition="('TOMH_UNICAN_Logi' in (items player))";
+				exceptions[]={};
+				icon = QPATHTOF(data\unican.paa);
+				#include "logican.hpp"
 			};
 		};
 	};
@@ -27,6 +34,15 @@ class CfgVehicles {
 		class TransportItems {
 			class _xx_Item {
 				name = "TOMH_UNICAN_Lite";
+				count = 1;
+			};
+		};
+	};
+	class TOMH_Item_UNICAN_Logi:TOMH_Item_UNICAN {
+		displayName = "UNICAN Logi";
+		class TransportItems {
+			class _xx_Item {
+				name = "TOMH_UNICAN_Logi";
 				count = 1;
 			};
 		};
---
+++ addons/main/logican.hpp
@@ -0,0 +1,81 @@
+#define STAT(Name) [_player, 'PATHTOF(spray\Name.paa)'] call ace_tagging_fnc_tag
+#define TAG(Name) \
+	class TOMH_##Name##_Tag { \
+		displayName = #Name; \
+		condition = "true"; \
+		statement = #STAT(Name); \
+		icon = QPATHTOF(spray\Name.paa); \
+	}
+
+class TOMH_UNICAN_A {
+	displayName = "A Station";
+	condition = "true";
+	TAG(AOOO);
+	TAG(AROO);
+	TAG(AORO);
+	TAG(AOOR);
+	TAG(ARRO);
+	TAG(AORR);
+	TAG(AROR);
+	TAG(ARRR);
+};
+class TOMH_UNICAN_B {
+	displayName = "B Station";
+	condition = "true";
+	TAG(BOOO);
+	TAG(BROO);
+	TAG(BORO);
+	TAG(BOOR);
+	TAG(BRRO);
+	TAG(BORR);
+	TAG(BROR);
+	TAG(BRRR);
+};
+class TOMH_UNICAN_C {
+	displayName = "C Station";
+	condition = "true";
+	TAG(COOO);
+	TAG(CROO);
+	TAG(CORO);
+	TAG(COOR);
+	TAG(CRRO);
+	TAG(CORR);
+	TAG(CROR);
+	TAG(CRRR);
+};
+class TOMH_UNICAN_D {
+	displayName = "D Station";
+	condition = "true";
+	TAG(DOOO);
+	TAG(DROO);
+	TAG(DORO);
+	TAG(DOOR);
+	TAG(DRRO);
+	TAG(DORR);
+	TAG(DROR);
+	TAG(DRRR);
+};
+class TOMH_UNICAN_E {
+	displayName = "E Station";
+	condition = "true";
+	TAG(EOOO);
+	TAG(EROO);
+	TAG(EORO);
+	TAG(EOOR);
+	TAG(ERRO);
+	TAG(EORR);
+	TAG(EROR);
+	TAG(ERRR);
+};
+class TOMH_UNICAN_F {
+	displayName = "F Station";
+	condition = "true";
+	TAG(FOOO);
+	TAG(FROO);
+	TAG(FORO);
+	TAG(FOOR);
+	TAG(FRRO);
+	TAG(FORR);
+	TAG(FROR);
+	TAG(FRRR);
+};
